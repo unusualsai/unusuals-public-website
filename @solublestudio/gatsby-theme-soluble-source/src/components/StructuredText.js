36934f200418ea5b4eb719b9b4648b77d0e06f25
Javier <jcorrochano@unusuals.ai>
2025-10-23 19:10:13 +0200

    Fix dependency issues: resolve Sharp and react-datocms compatibility
    - Fixed Sharp compilation issues in Netlify with environment variables
    - Restored react-datocms@1.6.6 (working version)
    - Updated yarn.lock with compatible dependency versions
    - Fixed StructuredText component compatibility issues
    - All builds now work without --ignore-engines flag
    
    Resolves Sharp Python 3.14 compilation errors and react-datocms StructuredText JavaScript errors

--- @solublestudio/gatsby-theme-soluble-source/src/components/StructuredText.js
+++ @solublestudio/gatsby-theme-soluble-source/src/components/StructuredText.js
@@ -1,11 +1,1 @@
-import React from 'react';
-import { StructuredText as DatoCMSStructuredText } from 'react-datocms';
-
-const StructuredText = (props) => {
-  return React.createElement(DatoCMSStructuredText, props);
-};
-
-// No intentamos asignar renderRule ya que no existe en el paquete correcto
-// StructuredText.renderRule = renderRule;
-
-export default StructuredText;
+export { StructuredText as default } from 'react-datocms';
