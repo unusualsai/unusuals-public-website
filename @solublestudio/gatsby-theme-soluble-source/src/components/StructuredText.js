1f3b9dd79c44a83c30f336cc702c5fe70d45d06f
Javier <jcorrochano@unusuals.ai>
2025-10-23 18:39:33 +0200

    Fix: Resolve Sharp compilation and StructuredText errors

--- @solublestudio/gatsby-theme-soluble-source/src/components/StructuredText.js
+++ @solublestudio/gatsby-theme-soluble-source/src/components/StructuredText.js
@@ -1,2 +1,11 @@
-import { StructuredText } from 'react-datocms';
+import React from 'react';
+import { StructuredText as DatoCMSStructuredText } from 'react-datocms';
+
+const StructuredText = (props) => {
+  return React.createElement(DatoCMSStructuredText, props);
+};
+
+// No intentamos asignar renderRule ya que no existe en el paquete correcto
+// StructuredText.renderRule = renderRule;
+
 export default StructuredText;
